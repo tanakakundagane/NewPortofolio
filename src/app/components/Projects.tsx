8003f0630d27a9a86d3c69ac3a070ce28bd92be5
Yosuke Iwata <s20970059@nucba.ac.jp>
2025-01-23 23:01:06 +0900

    projectsの作成

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { projects } from "../../data/projects"; // 配列をインポート
+import { AiFillBulb } from "react-icons/ai";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section className="px-6 py-10" id="projects">
+      <div className="bg-cadetblue text-white text-sm font-medium py-3 px-5 rounded-full flex items-center gap-2">
+        <p className="flex font-bold">
+          <span className="pr-1">
+            <AiFillBulb className="w-4 h-4" />
+          </span>
+          開発実績
+        </p>
+      </div>
 
-export default Projects
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-10">
+        {projects.map((project) => (
+          <a
+            key={project.id}
+            href={project.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group block relative"
+          >
+            <img
+              src={project.image}
+              alt={project.title}
+              className="w-full h-48 object-cover rounded-lg shadow-lg transition-transform duration-300 transform group-hover:scale-105"
+            />
+            <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center rounded-lg">
+              <p className="text-white font-semibold text-lg">
+                {project.title}
+              </p>
+            </div>
+          </a>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
---
+++ src/data/projects.ts
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    id: 1,
+    title: 'site1',
+    image: '/images/tennis.png', // 公開ディレクトリ内の画像
+    url: 'https://tennisschool.vercel.app/', // リンク先URL
+  },
+  {
+    id: 2,
+    title: 'site2',
+    image: '/images/zemi.png',
+    url: 'https://zemi-two.vercel.app/',
+  },
+  {
+    id: 3,
+    title: 'サイト3',
+    image: '/images/hotousagi.png',
+    url: 'https://hotousagi.nicework.co.jp/',
+  },
+  {
+    id: 4,
+    title: 'サイト4',
+    image: '/images/motion.png',
+    url: 'https://framer-motion-tutorial-mu.vercel.app/',
+  },
+  {
+    id: 5,
+    title: 'サイト5',
+    image: '/images/site5.png',
+    url: 'https://example5.com',
+  },
+  {
+    id: 6,
+    title: 'サイト6',
+    image: '/images/site6.png',
+    url: 'https://example6.com',
+  },
+];
