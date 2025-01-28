437ccf62f38b563d43e6deb88bbdbe0b13753794
Yosuke Iwata <s20970059@nucba.ac.jp>
2025-01-28 22:53:44 +0900

    Topbuttonコンポーネントの作成

--- src/app/components/TopButton.tsx
+++ src/app/components/TopButton.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import { FaArrowCircleUp } from 'react-icons/fa'
 
 const TopButton = () => {
+
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = ()=>{
+      if(window.scrollY > 300){
+        setScrolling(true)
+      }else{
+        setScrolling(false)
+      }
+    }
+    window.addEventListener('scroll',handleScroll)
+    return () =>{
+      window.removeEventListener('scroll',handleScroll)
+    }
+  })
+  
   return (
-    <div>TopButton</div>
+    <div className={
+      scrolling ? 'fixed bottom-[0.5rem] right-[0.7rem] z-50':'hidden'
+    }>
+      <a href="#home">
+      <FaArrowCircleUp className='text-cadetblue shadow-md rounded-full'
+      size={40}/>
+      </a>
+    </div>
   )
 }
 
